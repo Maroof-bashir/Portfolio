6cabc10d6f93b2805755a1342526e4bb0e024a81
Maroof Bashir <maroofbashir1v@gmail.com>
2025-06-02 12:18:31 +0530

    portfolio

---
+++ app.js
@@ -0,0 +1,33 @@
+let  links = document.getElementsByClassName("tablinks");
+let  contents = document.querySelectorAll(".tabcontents");
+
+
+// // function opentab(tabName){
+// //     for(link of links){
+// //         link.classlist.remove("active");
+// //     }
+// //     for(tabcontent of tabcontents){
+// //         content.Classlist.remove("active-tab");
+// //     }
+// // }
+// // console.log("hey!")
+
+// let skill = document.querySelectorAll("Skill")
+
+// skill.addEventListner("click",()=>{
+//     console.log("button was pressed ");
+// })
+function openTab(name){
+   for(link of links){
+    link.classList.remove("active");
+   }
+   for(content of contents){
+    content.classList.remove("active-tab");
+   }
+   event.currentTarget.classList.add("active");
+   // document.getElementById(name).classList.add("active-tab");           not working issue 
+   let arg = document.getElementById(name);
+   arg.classList.add("active-tab");
+
+    
+}
